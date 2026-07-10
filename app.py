6eeb78c0c3dbe3962f78596210074d1b4df92207
unknown <dbsehdrlf510@naver.com>
2026-07-10 17:31:30 +0900

    0710

--- app.py
+++ app.py
@@ -1,43 +1,60 @@
-from flask import Flask, render_template, request,send_file,redirect
-from scrapper import search_incruit
+from flask import Flask, render_template, request, send_file, redirect
+from scrapper import search_incruit, search_work24
 from file import save_to_csv
 
 app = Flask(__name__)
 
-db={}
-page=5
+db = {}
+page = 2
 
-@app.route('/')
-def hello_world():
+
+@app.route("/")
+def home():
     return render_template("index.html")
-
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    if keyword=="":
+
+    if keyword == "":
         return redirect("/")
-    
+
+    incruit_jobs = search_incruit(keyword, page)
+    work24_jobs = search_work24(keyword)
+
+    jobs = incruit_jobs + work24_jobs
+
+    db[keyword] = jobs
+
+    return render_template(
+        "result.html",
+        incruit_jobs=enumerate(incruit_jobs),
+        work24_jobs=enumerate(work24_jobs),
+        keyword=keyword,
+        count=len(jobs),
+        incruit_count=len(incruit_jobs),
+        work24_count=len(work24_jobs)
+    )
+
+
+@app.route("/file")
+def file():
+    keyword = request.args.get("keyword")
+
+    if keyword == "":
+        return redirect("/")
+
     if keyword in db:
         jobs = db[keyword]
     else:
-        jobs =search_incruit(keyword,page)
-        db[keyword]=jobs
+        jobs = search_incruit(keyword, page)
 
-    return render_template("search.html", jobs=enumerate(jobs),keyword=keyword,count=len(jobs))
-@app.route("/file")
-def file():
-    keyword=request.args.get("keyword")
-    if keyword=="":
-        return redirect("/")
-    if keyword in db:
-        jobs=db[keyword]
-    else:
-        jobs=search_incruit(keyword,page)
-    
     save_to_csv(jobs)
-    return send_file("./downloads.csv",as_attachment=True)
-    
+
+    return send_file(
+        "./downloads.csv",
+        as_attachment=True
+    )
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
--- file.py
+++ file.py
@@ -1,9 +1,9 @@
-import csv 
+import csv
 from scrapper import search_incruit
 
 def save_to_csv(jobs):
-     with open("./downloads.csv","w",encoding="cp949") as file:
-        csv_writer=csv.writer(file)
-        csv_writer.writerow(["No","회사","제목","지역","상세보기"])
-        for i,job in enumerate(jobs):
-            csv_writer.writerow([i+1,job["company"],job["title"],job["location"],job["link"]]) 
+   with open("./downloads.csv", "w", encoding="cp949") as file:
+      csv_writer = csv.writer(file)
+      csv_writer.writerow(["No", "회사", "제목", "지역", "상세보기"])
+      for i, job in enumerate(jobs):
+         csv_writer.writerow([i+1, job["company"], job["title"], job["location"], job["link"]])
